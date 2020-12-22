059eea0a1d173c11f9f0eef384d19e56faf81d5d
shykiu77 <carlosgabrielsns@gmail.com>
2020-12-22 14:52:48 -0300

    Adicionado gbtd

--- build/main.asm
+++ build/main.asm
@@ -9,7 +9,8 @@
 ; Public variables in this module
 ;--------------------------------------------------------
 	.globl _main
-	.globl _printf
+	.globl _set_sprite_data
+	.globl _sprite
 ;--------------------------------------------------------
 ; special function registers
 ;--------------------------------------------------------
@@ -17,6 +18,8 @@
 ; ram data
 ;--------------------------------------------------------
 	.area _DATA
+_sprite::
+	.ds 16
 ;--------------------------------------------------------
 ; absolute external ram data
 ;--------------------------------------------------------
@@ -28,6 +31,39 @@
 	.area _GSINIT
 	.area _GSFINAL
 	.area _GSINIT
+;..\src\/sprite.c:19: unsigned char sprite[] =
+	ld	hl, #_sprite
+	ld	(hl), #0x7e
+	ld	hl, #(_sprite + 0x0001)
+	ld	(hl), #0x7e
+	ld	hl, #(_sprite + 0x0002)
+	ld	(hl), #0x99
+	ld	hl, #(_sprite + 0x0003)
+	ld	(hl), #0x99
+	ld	hl, #(_sprite + 0x0004)
+	ld	(hl), #0x81
+	ld	hl, #(_sprite + 0x0005)
+	ld	(hl), #0x81
+	ld	hl, #(_sprite + 0x0006)
+	ld	(hl), #0xa5
+	ld	hl, #(_sprite + 0x0007)
+	ld	(hl), #0xa5
+	ld	hl, #(_sprite + 0x0008)
+	ld	(hl), #0x81
+	ld	hl, #(_sprite + 0x0009)
+	ld	(hl), #0x81
+	ld	hl, #(_sprite + 0x000a)
+	ld	(hl), #0xdb
+	ld	hl, #(_sprite + 0x000b)
+	ld	(hl), #0xdb
+	ld	hl, #(_sprite + 0x000c)
+	ld	(hl), #0xc3
+	ld	hl, #(_sprite + 0x000d)
+	ld	(hl), #0xc3
+	ld	hl, #(_sprite + 0x000e)
+	ld	(hl), #0x3c
+	ld	hl, #(_sprite + 0x000f)
+	ld	(hl), #0x3c
 ;--------------------------------------------------------
 ; Home
 ;--------------------------------------------------------
@@ -37,30 +73,40 @@
 ; code
 ;--------------------------------------------------------
 	.area _CODE
-;..\src\main.c:15: void main() {
+;..\src\main.c:4: void main() {
 ;	---------------------------------
 ; Function main
 ; ---------------------------------
 _main::
-;..\src\main.c:19: printf("Hello World!");
-	ld	hl, #___str_0
+;..\src\main.c:5: SPRITES_8x8;
+	ldh	a, (_LCDC_REG+0)
+	and	a, #0xfb
+	ldh	(_LCDC_REG+0),a
+;..\src\main.c:6: set_sprite_data(0, 8, sprite);
+	ld	hl, #_sprite
 	push	hl
-	call	_printf
-	add	sp, #2
-;..\src\main.c:22: printf("\n\nPress Start");
-	ld	hl, #___str_1
-	push	hl
-	call	_printf
-	add	sp, #2
-;..\src\main.c:23: }
+	ld	a, #0x08
+	push	af
+	inc	sp
+	xor	a, a
+	push	af
+	inc	sp
+	call	_set_sprite_data
+	add	sp, #4
+;C:/Users/carlo/Desktop/Periodo 7/IHS/Projeto Snake-Corona/tools/gbdk/include/gb/gb.h:999: shadow_OAM[nb].tile=tile;
+	ld	hl, #(_shadow_OAM + 0x0002)
+	ld	(hl), #0x00
+;C:/Users/carlo/Desktop/Periodo 7/IHS/Projeto Snake-Corona/tools/gbdk/include/gb/gb.h:1072: OAM_item_t * itm = &shadow_OAM[nb];
+	ld	hl, #_shadow_OAM
+;C:/Users/carlo/Desktop/Periodo 7/IHS/Projeto Snake-Corona/tools/gbdk/include/gb/gb.h:1073: itm->y=y, itm->x=x;
+	ld	a, #0x32
+	ld	(hl+), a
+	ld	(hl), #0x32
+;..\src\main.c:9: SHOW_SPRITES;
+	ldh	a, (_LCDC_REG+0)
+	or	a, #0x02
+	ldh	(_LCDC_REG+0),a
+;..\src\main.c:10: }
 	ret
-___str_0:
-	.ascii "Hello World!"
-	.db 0x00
-___str_1:
-	.db 0x0a
-	.db 0x0a
-	.ascii "Press Start"
-	.db 0x00
 	.area _CODE
 	.area _CABS (ABS)
--- src/main.c
+++ src/main.c
@@ -1,7 +1,10 @@
-
 #include <gb/gb.h>
-#include <stdio.h>
+#include "sprite.c"
+
 void main() {
-    printf("Hello World!");
-    printf("\n\nPress Start");
+    SPRITES_8x8;
+    set_sprite_data(0, 8, sprite);
+    set_sprite_tile(0, 0);
+    move_sprite(0, 50, 50);
+    SHOW_SPRITES;
 }
