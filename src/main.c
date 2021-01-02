32d409286ebe08087d37130614c3e4658ca7ae1b
shykiu77 <carlosgabrielsns@gmail.com>
2021-01-02 18:21:55 -0300

    Implementada a estrutura inicial do jogo e função para esconde sprites.

--- src/main.c
+++ src/main.c
@@ -2,6 +2,30 @@
 #include "ball.c"
 #include "bar.c"
 
+#define BALL 0
+#define L_BAR 1
+#define R_BAR 5
+
+#define TILE_SIZE 8
+
+void set_props(){
+    move_sprite(BALL,SCREENWIDTH/2,SCREENHEIGHT/2);
+
+    move_sprite(L_BAR,TILE_SIZE,SCREENHEIGHT/2 - TILE_SIZE);
+    move_sprite(L_BAR+1,TILE_SIZE,SCREENHEIGHT/2 - TILE_SIZE + TILE_SIZE);
+    move_sprite(L_BAR+2,TILE_SIZE,SCREENHEIGHT/2 - TILE_SIZE + 2*TILE_SIZE);
+    move_sprite(L_BAR+3,TILE_SIZE,SCREENHEIGHT/2 - TILE_SIZE + 3*TILE_SIZE);
+
+    move_sprite(R_BAR,SCREENWIDTH,SCREENHEIGHT/2 - TILE_SIZE);
+    move_sprite(R_BAR+1,SCREENWIDTH,SCREENHEIGHT/2 - TILE_SIZE + TILE_SIZE);
+    move_sprite(R_BAR+2,SCREENWIDTH,SCREENHEIGHT/2 - TILE_SIZE + 2*TILE_SIZE);
+    move_sprite(R_BAR+3,SCREENWIDTH,SCREENHEIGHT/2 - TILE_SIZE + 3*TILE_SIZE);
+}
+
+void hide(int sprite){
+    move_sprite(sprite,-10,-10);
+}
+
 void main(){
     SPRITES_8x8;
     set_sprite_data(0,0,ball);
@@ -24,16 +48,7 @@ void main(){
     set_sprite_tile(7,7);
     set_sprite_tile(8,8);
 
-    move_sprite(0,128,128);
-
-    move_sprite(1,20,50);
-    move_sprite(2,20,58);
-    move_sprite(3,20,66);
-    move_sprite(4,20,74);
-
-    move_sprite(5,56,50);
-    move_sprite(6,56,58);
-    move_sprite(7,56,66);
-    move_sprite(8,56,74);
+    set_props();
+    hide(BALL);
     SHOW_SPRITES;
 }
