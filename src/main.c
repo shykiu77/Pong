f362401dade447fccf217583d26faaebefcbb877
ericksousa77 <erickss@dcomp.ufs.br>
2021-01-05 19:49:36 -0300

    Movimentacao das barras

--- src/main.c
+++ src/main.c
@@ -1,6 +1,8 @@
 #include <gb/gb.h>
 #include "ball.c"
 #include "bar.c"
+#include <gb/drawing.h>
+#include <stdio.h>
 
 #define BALL 0
 #define L_BAR 1
@@ -8,11 +10,23 @@
 
 #define TILE_SIZE 8
 
-WORD ball_x = SCREENWIDTH/2;
-WORD ball_y = SCREENHEIGHT/2;
-
 WORD x_velocity = 2;
 WORD y_velocity = 0;
+
+WORD pad;
+WORD gameover = 1;
+WORD i = 1;
+WORD y = SCREENHEIGHT/2 - TILE_SIZE;
+
+//propriedades da bola
+WORD ball_x = SCREENWIDTH/2;
+WORD ball_y = SCREENHEIGHT/2;
+WORD ball_dx = 1;
+WORD ball_dy = 1;
+
+
+WORD action = 3;
+
 
 void update_ball(){
     ball_x += x_velocity;
@@ -23,6 +37,31 @@ void update_ball(){
     }
     move_sprite(BALL,ball_x,ball_y);
 }
+
+void move_bar() {
+
+	// if the up arrow is pressed move paddle up
+	if (action == 1) {
+
+	    move_sprite(L_BAR,TILE_SIZE,y-i);
+        move_sprite(L_BAR+1,TILE_SIZE,y + TILE_SIZE-i);
+        move_sprite(L_BAR+2,TILE_SIZE,y + 2*TILE_SIZE-i);
+        move_sprite(L_BAR+3,TILE_SIZE,y + 3*TILE_SIZE-i);
+        y -= i;
+        action = 3;
+	}
+    if (action == 0) {
+
+	    move_sprite(L_BAR,TILE_SIZE,y+i);
+        move_sprite(L_BAR+1,TILE_SIZE,y + TILE_SIZE+i);
+        move_sprite(L_BAR+2,TILE_SIZE,y + 2*TILE_SIZE+i);
+        move_sprite(L_BAR+3,TILE_SIZE,y + 3*TILE_SIZE+i);
+        y+= i;
+        action = 3;
+	}
+}
+
+
 
 void set_props(){
     ball_x = SCREENWIDTH/2;
@@ -39,11 +78,13 @@ void set_props(){
     move_sprite(R_BAR+2,SCREENWIDTH,SCREENHEIGHT/2 - TILE_SIZE + 2*TILE_SIZE);
     move_sprite(R_BAR+3,SCREENWIDTH,SCREENHEIGHT/2 - TILE_SIZE + 3*TILE_SIZE);
 }
-
 void hide(int sprite){
     move_sprite(sprite,-10,-10);
 }
 
+void loadMenu(){
+    
+}
 
 
 void main(){
@@ -71,8 +112,21 @@ void main(){
     set_props();
     SHOW_SPRITES;
     while(1){
+        pad = joypad();
+        if (pad & J_UP){
+            action = 1;
+            move_bar();
+        }
+        if (pad & J_DOWN){
+            action = 0;
+            move_bar();
+        }
+        
         wait_vbl_done();
         delay(20);
+        //drawUI();
         update_ball();
+        //gameover = 0;
+        
     }
 }
