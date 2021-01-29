1548584c25cf1ea88af70aea5e26209dbda039e5
shykiu77 <carlosgabrielsns@gmail.com>
2021-01-29 16:06:43 -0300

    consertado um erro de colisão

--- src/main.c
+++ src/main.c
@@ -172,7 +172,7 @@ void check_collision()
   {
     
     if(fix < 0){
-      score_left++;
+      score_right++;
       mark_score_right();
       delay(300);
     }
@@ -193,12 +193,15 @@ void check_collision()
   if (ball_x - RADIUS_BALL <= LEFT_LIMIT && ball_y >= yl - RADIUS_BALL && ball_y <= yl + 4 * TILE_SIZE + RADIUS_BALL)
   {
     x_velocity = -x_velocity;
-    if (ball_y - yl < 8)
+    if (ball_y - yl < 5){
       y_velocity = -1;
-    else if (ball_y - yl > 16)
+    }
+    else if (ball_y - yl > 16){
       y_velocity = 1;
-    else
+    }
+    else{
       y_velocity = 0;
+    }
     return;
   }
 
@@ -206,12 +209,15 @@ void check_collision()
   if (ball_x + RADIUS_BALL >= RIGHT_LIMIT && ball_y >= yr - RADIUS_BALL && ball_y <= yr + 4 * TILE_SIZE + RADIUS_BALL)
   {
     x_velocity = -x_velocity;
-    if (ball_y - yr < 8)
+    if (ball_y - yr < 5){
       y_velocity = -1;
-    else if (ball_y - yr > 16)
+    }
+    else if (ball_y - yr > 16){
       y_velocity = 1;
-    else
+    }
+    else{
       y_velocity = 0;
+    }
     return;
   }
 }
@@ -312,7 +318,6 @@ void main()
         send_action(0);
       }
       delay(10);
-      
       move_bar(!side,validade_data());
       _io_in = 3;
 
